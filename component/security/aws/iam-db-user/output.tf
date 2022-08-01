d643dcaabd7a2d904c8847a7858a5eba3f0c5509
Kanchi Kumar <106963917+Kanchikumar-glenise@users.noreply.github.com>
2022-08-01 21:12:55 +0530

    Update output.tf

--- component/security/aws/iam-db-user/output.tf
+++ component/security/aws/iam-db-user/output.tf
@@ -2,8 +2,3 @@ output "user_arn" {
   value       = aws_iam_user.iam_user.*.arn
   description = "User ARN"
 }
-
-output "group_arn" {
-  value       = aws_iam_group.iam_group.arn
-  description = "Group ARN"
-}
