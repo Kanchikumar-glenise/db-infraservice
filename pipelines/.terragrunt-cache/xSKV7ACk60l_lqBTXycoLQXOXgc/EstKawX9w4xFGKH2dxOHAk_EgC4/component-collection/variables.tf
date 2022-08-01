45afbccf6024365e2645e426f348bddd5223b165
Naveen Kumar <naveenkumar@Naveens-MacBook-Pro.local>
2022-08-01 18:53:33 +0100

    moved variables

--- pipelines/.terragrunt-cache/xSKV7ACk60l_lqBTXycoLQXOXgc/EstKawX9w4xFGKH2dxOHAk_EgC4/component-collection/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "AWS_DEFAULT_REGION" {
-  default = "eu-west-2"
-}
-
-variable "AWS_ACCESS_KEY_ID" {}
-
-variable "AWS_SECRET_ACCESS_KEY" {}
--- pipelines/.terragrunt-cache/xSKV7ACk60l_lqBTXycoLQXOXgc/EstKawX9w4xFGKH2dxOHAk_EgC4/component/security/aws/iam-db-user/output.tf
+++
@@ -1,4 +0,0 @@
-output "user_arn" {
-  value       = aws_iam_user.iam_user.*.arn
-  description = "User ARN"
-}
