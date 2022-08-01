0c5c4e463e48b32f932a6360a2b2f2dc42eba0f3
Kanchi Kumar <106963917+Kanchikumar-glenise@users.noreply.github.com>
2022-08-01 21:12:40 +0530

    Update main.tf

--- component/security/aws/iam-db-user/main.tf
+++ component/security/aws/iam-db-user/main.tf
@@ -1,20 +1,3 @@
-resource "aws_iam_group" "iam_group" {
-  name = var.group_name
-}
-
-resource "aws_iam_group_membership" "iam_group_membership" {
-  name = "iam-group-membership-${var.name}"
-
-  users = tolist(aws_iam_user.iam_user.*.name)
-
-  group = aws_iam_group.iam_group.name
-}
-
-resource "aws_iam_group_policy_attachment" "policy_attachment" {
-  group      = aws_iam_group.iam_group.name
-  policy_arn = var.policy_arn
-}
-
 resource "aws_iam_user" "iam_user" {
   count = length(var.users)
   name  = var.users[count.index]
